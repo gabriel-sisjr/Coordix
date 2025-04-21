968f8519e220b0fac161674ec846378dc1a4e7b4
Gabriel Santana <gabriel.sistemasjr@gmail.com>
2025-04-22 00:05:00 +0100

    feat: Simple sample, everything at same level no DI

---
+++ examples/SimpleSample/SimpleSample/PingHandler.cs
@@ -0,0 +1,9 @@
+﻿using Coordix.Interfaces;
+
+namespace SimpleSample;
+
+public sealed class PingHandler : IRequestHandler<Ping, string>
+{
+    public async Task<string> Handle(Ping request, CancellationToken cancellationToken)
+        => await Task.FromResult($"Pong: {request.Message}");
+}
