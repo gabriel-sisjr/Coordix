d6f6583b4d86c6122478b75fe8eb5d1c86c89d84
Gabriel Santana <gabriel.sistemasjr@gmail.com>
2025-11-21 01:12:35 +0000

    feat: add DI extensions for CodeGen integration
    - Add AddCoordixWithCodeGen extension method for service registration
    - Automatically locate and register GeneratedHandlerExecutor at runtime
    - Replace reflection-based executor with generated implementation
    - Support HandlerResolutionMode.CodeGenPreferred configuration

---
+++ src/CodeGen/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using Coordix.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Coordix.CodeGen.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+	private const string GeneratedExecutorTypeName = "Coordix.Implementation.GeneratedHandlerExecutor";
+
+	public static IServiceCollection AddCoordixWithCodeGen(this IServiceCollection services)
+	{
+		return AddCoordixWithCodeGen(services, configureOptions: null);
+	}
+
+	public static IServiceCollection AddCoordixWithCodeGen(
+		this IServiceCollection services,
+		Action<CoordixOptions>? configureOptions)
+	{
+		return AddCoordixWithCodeGen(services, configureOptions, Array.Empty<object>());
+	}
+
+	public static IServiceCollection AddCoordixWithCodeGen(
+		this IServiceCollection services,
+		Action<CoordixOptions>? configureOptions,
+		params object[] args)
+	{
+		var generatedExecutorType = FindGeneratedExecutorType();
+
+		if (generatedExecutorType == null)
+		{
+			throw new InvalidOperationException(
+				$"Generated HandlerExecutor type '{GeneratedExecutorTypeName}' was not found. " +
+				"Make sure the Coordix.CodeGen source generator is properly installed and the project has been built.");
+		}
+
+		Coordix.Extensions.ServiceCollectionExtensions.AddCoordix(
+			services,
+			options =>
+			{
+				options.HandlerResolutionMode = HandlerResolutionMode.Reflection;
+				configureOptions?.Invoke(options);
+			},
+			args);
+
+		services.RemoveAll<IHandlerExecutor>();
+		services.AddSingleton(typeof(IHandlerExecutor), generatedExecutorType);
+
+		services.RemoveAll<CoordixOptions>();
+		var finalOptions = new CoordixOptions
+		{
+			HandlerResolutionMode = HandlerResolutionMode.CodeGenPreferred
+		};
+		configureOptions?.Invoke(finalOptions);
+		services.AddSingleton(finalOptions);
+
+		return services;
+	}
+
+	private static Type? FindGeneratedExecutorType()
+	{
+		var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+		foreach (var assembly in assemblies)
+		{
+			try
+			{
+				var type = assembly.GetType(GeneratedExecutorTypeName);
+				if (type != null && typeof(IHandlerExecutor).IsAssignableFrom(type))
+				{
+					return type;
+				}
+			}
+			catch
+			{
+				continue;
+			}
+		}
+
+		return null;
+	}
+}
+
